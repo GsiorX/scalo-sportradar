62adf2c07531cd2e8b10fe5713451f66ac49312f
Jakub Gasiorek <jakub.gasiorek@efficioconsulting.com>
2023-08-14 08:50:55 +0200

    Update GameTest with new assumptions and validation

--- tests/unit/GameTest.php
+++ tests/unit/GameTest.php
@@ -22,10 +22,9 @@ final class GameTest extends TestCase
         $team2 = new Team();
         $team2->setName($team2Name);
 
-        $game = new Game(
-            $team1,
-            $team2,
-        );
+        $game = new Game();
+        $game->setHomeTeam($team1);
+        $game->setAwayTeam($team2);
 
         // When I get the names of the teams
         $homeTeamName = $game->getHomeTeamName();
@@ -48,10 +47,9 @@ final class GameTest extends TestCase
         $team2 = new Team();
         $team2->setName($team2Name);
 
-        $game = new Game(
-            $team1,
-            $team2,
-        );
+        $game = new Game();
+        $game->setHomeTeam($team1);
+        $game->setAwayTeam($team2);
 
         // When I get the score of the game
         $homeTeamScore = $game->getHomeTeamScore();
@@ -74,10 +72,9 @@ final class GameTest extends TestCase
         $team2 = new Team();
         $team2->setName($team2Name);
 
-        $game = new Game(
-            $team1,
-            $team2,
-        );
+        $game = new Game();
+        $game->setHomeTeam($team1);
+        $game->setAwayTeam($team2);
 
         // When I update the score of the game
         $game->updateScore(2, 1);
@@ -89,5 +86,47 @@ final class GameTest extends TestCase
         $this->assertSame(2, $homeTeamScore);
         // Then I expect the away team's score to be 1
         $this->assertSame(1, $awayTeamScore);
+    }
+
+    public function testUpdateScoreWithNegativeValueFails(): void
+    {
+        // Given I have a game with two teams
+        $team1Name = 'Team 1';
+        $team2Name = 'Team 2';
+
+        $team1 = new Team();
+        $team1->setName($team1Name);
+        $team2 = new Team();
+        $team2->setName($team2Name);
+
+        $game = new Game();
+        $game->setHomeTeam($team1);
+        $game->setAwayTeam($team2);
+
+        // Then I expect the exception to be thrown
+        $this->expectException(NegativeScoreException::class);
+
+        // When I update the score of the game
+        $game->updateScore(-2, -1);
+    }
+
+    public function testSettingSameTeamNameTwiceFails(): void
+    {
+        // Given I have a game with two teams
+        $team1Name = 'Team 1';
+        $team2Name = 'Team 1';
+
+        $team1 = new Team();
+        $team1->setName($team1Name);
+        $team2 = new Team();
+        $team2->setName($team2Name);
+
+        // Then I expect the exception to be thrown
+        $this->expectException(NonUniqueTeamNameException::class);
+
+        // When I set the names of the teams
+        $game = new Game();
+        $game->setHomeTeam($team1);
+        $game->setAwayTeam($team2);
     }
 }
